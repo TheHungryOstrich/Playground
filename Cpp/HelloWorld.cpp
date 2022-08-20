bcbccb4a716b95bf05332e3609761d98cf0afedd
TheHungryOstrich <sandeep.kulkarni.412@gmail.com>
2022-08-20 13:10:38 +0100

    C to cpp

--- Cpp/HelloWorld.cpp
+++ Cpp/HelloWorld.cpp
@@ -1,8 +1,8 @@
-#include <stdio.h>
+// Your First C++ Program
 
-int main()
-{
-    printf("Hello, world?");
-    
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
     return 0;
 }
